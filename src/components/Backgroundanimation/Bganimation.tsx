9183f78abe6faca725243e4ea848fd648cd66ac8
SameerKhans13 <sam13sameer.13khan@gmail.com>
2024-11-28 19:59:37 +0530

    Completed the Background Animation

--- src/components/Backgroundanimation/Bganimation.tsx
+++ src/components/Backgroundanimation/Bganimation.tsx
@@ -1,32 +1,53 @@
-import React from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import { motion, useScroll, useSpring } from "framer-motion";
 import "./bganimation.css";
+import { pre } from "framer-motion/client";
 
 const ParticleBackground: React.FC = () => {
   const particles = Array.from({ length: 50 });
+  const pwrapRef = useRef<any>(null);
+  const [dimension, setDimension] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    if (
+      pwrapRef.current.offsetWidth != null &&
+      pwrapRef.current.offsetHeight != null
+    ) {
+      setDimension({
+        width: pwrapRef.current.offsetWidth,
+        height: pwrapRef.current.offsetHeight,
+      });
+    }
+  }, [pwrapRef]);
 
   return (
-    <div className="particles-wrapper">
-      {/* {particles.map((_, index) => (
-        <motion.div
-          key={index}
-          className="particle"
-          initial={{
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight,
-          }}
-          animate={{
-            x: Math.random() * window.innerWidth,
-            y: Math.random() * window.innerHeight,
-          }}
-          transition={{
-            duration: Math.random() * 10 + 5,
-            repeat: Infinity,
-            repeatType: "reverse",
-            ease: "linear",
-          }}
-        />
-      ))} */}
+    <div className="particles-wrapper" ref={pwrapRef}>
+      {dimension.width != 0 && dimension.height != 0
+        ? particles.map((element, index) => (
+            <motion.div
+              key={index}
+              className="particle"
+              initial={{
+                x: Math.random() * dimension.width,
+                y: Math.random() * dimension.height,
+              }}
+              animate={{
+                x: Math.random() * dimension.width,
+                y: Math.random() * dimension.height,
+              }}
+              transition={{
+                duration: Math.random() * 10 + 5,
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "linear",
+              }}
+            />
+          ))
+        : null}
     </div>
   );
 };
